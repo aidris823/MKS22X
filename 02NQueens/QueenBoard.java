f4c23b06b843e23bd1e8f6b69198eb5c2e3c697d
aidris823 <aidris@stuy.edu>
2018-02-13 23:13:42 -0500

    Probable estas inteligenta se mi ne usas i insteaw row...

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -149,18 +149,21 @@ public class QueenBoard {
 	if (col == size){
 	    return true;
 	}
-	//i = each row
-	for (int i = 0; i < size; i++){
-	    if (addQueen(i,col)){
+
+	for (int row = 0; i < size; i++){
+	    if (addQueen(row,col)){
 		if (solveHelper(col+1)){
-			return true;
+		    return true;
 		}
 	    }
 	    removeQueen(i,col);
 	}
 	return false;
     }
-    
+
+    public int countSolutions(){
+	
+    }
 	
     public static void main(String[] arguments) {
 	QueenBoard daBoard = new QueenBoard(8);
@@ -170,5 +173,36 @@ public class QueenBoard {
 	daBoard.addQueen(0,4);
 	System.out.println(daBoard.toString());
 	System.out.println(daBoard.threatCheck());
+	QueenBoard b = new QueenBoard(4);
+
+	System.out.println(b.solve()); //prints true
+	System.out.println(b); //prints a valid solution
+
+	try{
+	    b.solve();
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+	try{
+	    b.countSolutions();
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+	for (int i = 0; i < 12; i++){
+	    QueenBoard a = new QueenBoard(i);
+	    System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+	    /*          Expected Values
+			i --> # of Solutions   i --> # of Solutions
+			0 --> 1                      6 --> 4
+			1 --> 1                      7 --> 40
+			2 --> 0                      8 --> 92
+			3 --> 0                      9 --> 352
+			4 --> 2                    10 --> 724
+			5 --> 10                  11 --> 2680
+	    */
+	    System.out.println(a); //prints out an empty i by i grid of underscores
+	}
     }
 }
