068e00bdc599697336e42e45847b93b94f70af71
aidris823 <aidris@stuy.edu>
2018-02-27 22:35:44 -0500

    Solve is kinda working? I dont know

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,7 +2,9 @@ public class KnightBoard{
     private int[][] board;
     private int rowSize;
     private int colSize;
-    private boolean solveCalled;
+    private int[] pR = new int[]{-2, -2, 1, 1, 2, 2, -1,-1};
+    private int[] pC = new int[]{ 1, -1, 2,-2, 1,-1, -2, 2};
+
     private int[] prevCoordinates = new int[]{-1,1};
     
     //Constructor:
@@ -13,7 +15,6 @@ public class KnightBoard{
 	}
 	rowSize = startingRows;
 	colSize = startingCols;
-	solveCalled = false;
 	board = new int[rowSize][colSize];
 	resetBoard(rowSize,colSize);
     }
@@ -26,7 +27,9 @@ public class KnightBoard{
 	    }	}
     }
 
+    //Solves Knight tour?
     public boolean solve(int startingRow, int startingCol){
+	
 	for (int i = 0; i < rowSize; i++){
 	    for (int j = 0; j < colSize; j++){
 		if (board[i][j] != 0){
@@ -34,27 +37,111 @@ public class KnightBoard{
 		}
 	    }
 	}
-	if (!isInsideBounds(startingRow) || !isInsideBounds(startingCols)){
+	if (!isWithinBounds(startingRow,startingCol)){
 	    throw new IllegalArgumentException();
 	}
+	
 	return solveHelper(startingRow, startingCol, 1);
     }
     public boolean solveHelper(int row, int col, int level){
-	
+
+	//Base cases
+	if (level == row * col && board[row][col] == 0){
+	    board[row][col] = level;
+	    return true;
+	}
+	if (!hasLegalSquare(row,col)){
+	    return false;
+	}
+	for (int i = 0; i < 8; i++){
+	    if (isWithinBounds(row + pR[i],col+pC[i])){
+		board[row][col] = level;
+		if (solveHelper(row + pR[i],col + pC[i], level + 1)){
+		    return true;
+		}
+		else{
+		board[row][col] = 0;
+		}
+	    }
+	}
+	return false;
+		    
     }
+       
+	
 
     //Helper functions that determines if a move is within bounds:
     private boolean isWithinBounds(int r, int c){
 	return (r > 0 && c > 0 && r < rowSize && c < colSize);
     }
 
+    //Sees if Knight currently has a legal square.
+    private boolean hasLegalSquare(int r, int c){
+
+	for (int i = 0; i < 8; i++){
+	    if (isWithinBounds(r+pR[i],c+pC[i]) && (board[r+pR[i]][c+pC[i]] == 0)){
+		return true;
+	    }
+	}
+	return false;
+    }
+    /*
+
+    //Returns an array of legal moves for the Knight.
+    private int[][] getLegalMoves(int r, int c){
+	/*Having eight legal moves is
+	only possible if it's the first move for the
+	Knight and it's not near a corner, 
+	but it's just to be safe.
+	
+	int[][] ans = new int[8][1];
+	int[] legalMove = new int[2];
+	//pR = "Possible R", and so on for C. 
+	int[] pR = new int[]{-2, -2, 1, 1, 2, 2, -1,-1};
+	int[] pC = new int[]{ 1, -1, 2,-2, 1,-1, -2, 2};
+	for (int i = 0; i < 8; i++){
+	    if (isWithinBounds(r+pR[i],c+pC[i])
+		&& (board[r+pR[i]][c+pC[i]] == 0)){
+		legalMove[0] = r+pR[i];
+		legalMove[1] = c+pC[i];
+		ans[i] = legalMove;
+	    }
+	}
+	return ans;
+    }
+    */
+    public boolean isSolvable(){
+	return !(rowSize == 2 || colSize == 2 || (rowSize == 3 && colSize == 3) || rowSize == 1 || colSize == 1);
+	}
+	
+	
         
+    public String toString(){
+	String ans = "";
+	for (int i = 0; i < rowSize; i++){
+	    for (int j = 0; j < colSize; j++){
+		if (board[i][j] == 0 || !isSolvable()){
+		    ans += "_ ";
+		}
+		else if (board[i][j] > 0 && board[i][j] < 10){
+		    ans += " " + board[i][j];
+		}
+		else{
+		    ans += board[i][j];
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
 
-    public String toString(){}
+    public static void main(String[] arguments){
+	//I'm trying a 5 x 5 first and starting on [2,2]...because Wikipedia has an animation for that Knight tour.
+	KnightBoard kasparov = new KnightBoard(5,5);
+	kasparov.solve(2,2);
+	System.out.println(kasparov.toString());
+    }
 
-       
-
-    
 }
 	       
 		
