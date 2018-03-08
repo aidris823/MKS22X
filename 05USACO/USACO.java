ab4555af6323bcaaa67152886db13a8c50343622
aidris823 <pokeanand@gmail.com>
2018-03-08 15:24:37 -0500

    Did scanning code.

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,6 +1,39 @@
 public class USACO{
     public static int bronze(String filename){
     }
+    
     public static int silver(String filename){
+	int[] start;
+	int[] end;
+	char[][] map;
+	int[][] currentNumofPaths;
+	int[][] oldNumOfPaths;
+	int rowCount = 0;
+	int colCount = 0;
+
+	try{
+	    File f = new File(filename);
+	    Scanner in = new Scanner(f);
+	    while (in.hasNextLine()){
+		String line = in.nextLine();
+		colCount = line.length();
+		rowCount++;
+	    }
+	    map = new char[rowCount][colCount];
+	    in = new Scanner(f);
+	    int counter = 0;
+	    while (in.hasNextLine()){
+		String line = in.nextLine();
+		for (int i = 0; i < line.length(); i++){
+		    map[counter][i] = line.charAt(i);
+		}
+		counter++;
+	    }
+	}
+	catch (FileNotFoundException e){
+	    System.out.println("File not found: " + filename);
+	    System.exit(1);
+	}
+	
     }
 }
