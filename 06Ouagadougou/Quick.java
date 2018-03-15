5339c77030bfa1f1fce879a11ea3059680c0f8e2
aidris823 <aidris@stuy.edu>
2018-03-15 01:02:49 -0400

    Okay, I think partition works

--- 06Ouagadougou/Quick.java
+++ 06Ouagadougou/Quick.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
 public class Quick{
     public static int partition(int[] data, int start, int end){
-	int pivot = (int) end - ( Math.random() * start);
-	i = start - 1;
+	/*Used code from here: https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values 
+after I couldn't think of way to generate number between start and end without 
+lousy conversion thing. */	
+	int pivot = (int) Math.random() * end + start;
+	// System.out.println("Pivot: " + pivot);
 	int i = start + 1;
 	int j = end;
 	while (i <= j){
@@ -14,15 +18,20 @@ public class Quick{
 	    }
 	}
 	swap(data,i,j);
-	return j;
-		
-		
-	}
-	
-    }
+	return j;				
+    }	
     public static void swap(int[] data, int lo, int hi){
+	int x = data[lo];
+	data[lo] = data[hi];
+	data[hi] = x;
     }
-    
+    public static void main(String[] arguments){
+
+	int[] mbwe = {999,999,999,4,1,0,3,2,999,999,999};
+	System.out.println(Arrays.toString(mbwe));
+	System.out.println(Quick.partition(mbwe, 0, 10));
+	System.out.println(Arrays.toString(mbwe));
+    }
 	    
 }	
 		
