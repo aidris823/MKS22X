c7b81104255fe55722d24156d1be732bc16df184
aidris823 <pokeanand@gmail.com>
2018-03-20 15:17:37 -0400

    Started MergeSort.

---
+++ 07Merge/Merge.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Merge{
+
+    public static void mergesort(int[] data){
+	System.out.println("Current Array: " + Arrays.toString(data));
+	int[] x = new int[data.length];
+	msort(data, x, 0, data.length - 1);
+	System.out.println("Sorted Array: " + Arrays.toString(data));
+    }
+
+    public static void msort(int[] data, int[] temp, int lo, int hi){
+	if (lo >= hi){
+	    return;
+	}
+	for (int i = low; i < hi; i++){
+	    
+	}
+    }
+
+    public 
+       
