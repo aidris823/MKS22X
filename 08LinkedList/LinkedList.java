55f2d9c3a3f8aae73323f1c596ad4a5b4ea99802
aidris823 <pokeanand@gmail.com>
2018-03-28 15:10:33 -0400

    Started LinkedList

---
+++ 08LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+public class LinkedList implements List{
+    private Node start, end;
+    private int size;
+
+    private Node getNode(int i){
+    }
+    
+}
+
+private class Node{
+    private int data;
+    private Node next, prev;
+    void setData(int x){
+    }
+    int getData(){
+    }
+    void setPrev(int x){
+    }
+    int getPrev(){
+    }
+    void setNext(int x){
+    }
+    int getNext(){
+    }
+}
