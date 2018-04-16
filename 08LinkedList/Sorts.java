14623058aaea5c75b3992b237836c02eda9bd181
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-04-16 03:51:22 -0400

    Nyeh heh heh (nope)

--- 08LinkedList/Sorts.java
+++ 08LinkedList/Sorts.java
@@ -1,3 +1,18 @@
+import java.util.*;
 public class Sorts{
-    public static void radixsort(MyLinkedListImproved<Integer> data){
+    public static void radixsort (MyLinkedListImproved<Integer> data){
+	@SupressWarnings("unchecked")
+	    MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
+
+	//Avoid NullPointerException
+	for (int i = 0; c < bucket.length; i++){
+	    bucket[i] = new MyLinkedListImproved<Integer>();
+	}
+
+	int max = data.max();
+	int numRuns = (max + "").length();
+
+	    
     }
+}
+	
