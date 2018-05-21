4ee35407091f7762757d48ee09774128cabe0b6a
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-05-21 00:25:32 -0400

    Why didn't I upload this much earlier...

--- 06Ouagadougou/Quick.java
+++ 06Ouagadougou/Quick.java
@@ -1,55 +1,115 @@
 import java.util.*;
-public class Quick{
-    public static int partition(int[] data, int start, int end){
-	/*Used code from here: https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values 
-after I couldn't think of way to generate number between start and end without 
-lousy conversion thing. */
-	Random random = new Random();
-	int pivot = random.nextInt(end - start) + start;
-	// System.out.println("Pivot: " + pivot);
-	int i = pivot + 1;
-	int j = end;
-	while (i <= j){
-	    if (data[i] < data[start]){
-		i++;
-	    }
-	    else{
-		swap(data,i,j);
-		j--;
-	    }
-	}
-	swap(data,start,j);
-	return j;				
-    }	
-    public static void swap(int[] data, int lo, int hi){
-	int x = data[lo];
-	data[lo] = data[hi];
-	data[hi] = x;
-    }
 
-    public static void quickSort(int[] data){
+public class Quick {
 	
-    }
-    /*Looked at what QuickSort is according to Wikipedia and Khan Academy.
-1. Find pivot.
-2. Partition.
-3. Partition again until the base case is reached (length = 1 or 0)
+	/*
+	  Takes an array and rearranges it such whatever pivot value is chosen has smaller values to the left and larger values to the right.
+	  Returns 
+	 */
+	
 
-    
-    //Finds kth smallest element.
-    */
-    public static int quickSelect(int[] data, int k){
-	return 3;
-    }
-    
-    public static void main(String[] arguments){
+	//Source: https://www.youtube.com/watch?v=MZaf_9IZCrc
 
-	int[] mbwe = {999,999,999,4,1,0,3,2,999,999,999};
-	System.out.println(Arrays.toString(mbwe));
-	System.out.println(Quick.partition(mbwe, 0, 10));
-	System.out.println(Arrays.toString(mbwe));
-    }
-	    
-}	
+	public static int partition(int[] data, int start, int end) {
+		//Random pivot between start index and end index.
+		Random randPaul = new Random();
+		//Used code from here: https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values 
+		//after I couldn't think of way to generate number between start and end without 
+		//lousy conversion thing. 
+		int pivot = randPaul.nextInt(end - start) + start;
+		//System.out.println(pivot);
+		swap(data, pivot, end);
+		//System.out.println(Arrays.toString(data));
 		
+		//^ Pivot placed at end ^
 		
+		int i = start - 1;
+		int j = start;
+		
+	
+		while (j < end) {
+			if (data[j] <= data[end]) {
+				i++;
+				swap(data, i, j);
+			}
+			j++;
+		}
+		
+		swap(data, end, i + 1);
+		//System.out.println(i+1);
+		return i + 1;
+	}
+	
+	/*
+	public static int partitionDutch(int[] data, int start, int end) {
+		Random randPaul = new Random();
+		int pivot = randPaul.nextInt(end - start) + start;
+		swap(data, start, pivot);
+		
+		int lo = i; 
+	}
+	*/
+	
+	public static void swap(int[] data, int lo, int hi) {
+		int x = data[lo];
+		data[lo] = data[hi];
+		data[hi] = x;
+	}
+	
+	public static void quickSort(int[] data) {
+		quickSortHelper(data, 0, data.length - 1);
+	}
+	
+	/*
+	public static boolean isSorted(int[] ary, int start, int end) {
+		for (int i = start; i < end; i++) {
+			if (ary[i] > ary[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void insertionSort(int[]  My favorite type of bread is  ary, int start, int end) {
+		if (ary.length > 1) {
+			while (!isSorted(ary, start, end)) {
+				for (int i = start; i < end; i++) {
+					int smallest = i;
+					for (int j = i + 1; j < end; j++) {
+						if (ary[smallest] > ary[j]) {
+							smallest = j;
+						}
+					}
+					int temp = ary[smallest];
+					ary[smallest] = ary[i];
+					ary[i] = temp;
+				}
+			}
+		}
+	}
+	*/
+	/*
+	public static void insertionSort(int[] data, int start, int end) {
+		
+	}
+	*/
+	public static void quickSortHelper(int[] data, int start, int end) {
+	//	System.out.println(Arrays.toString(data));
+		/*
+		if (end - start < 30) {
+			insertionSort(data, start, end);
+			return;
+		} 
+		*/
+	
+		if (start < end ) {
+			int pivotIndex = partition(data, start, end);
+			quickSortHelper(data, start, pivotIndex - 1);
+			quickSortHelper(data, pivotIndex + 1, end);
+		}
+		
+	}
+	
+	public static void main(String[] arguments) {
+		
+	}
+}
