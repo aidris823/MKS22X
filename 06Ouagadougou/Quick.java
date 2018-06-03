600ef1d5b1806805d4b5e98558263a0e32cbc7e8
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-06-03 05:05:29 -0400

    Completely works

--- 06Ouagadougou/Quick.java
+++ 06Ouagadougou/Quick.java
@@ -40,15 +40,75 @@ public class Quick {
 		return i + 1;
 	}
 	
-	/*
+	//Opposite direction than other Partition (Start to End)
 	public static int partitionDutch(int[] data, int start, int end) {
 		Random randPaul = new Random();
 		int pivot = randPaul.nextInt(end - start) + start;
 		swap(data, start, pivot);
 		
-		int lo = i; 
+		int lt, gt, i;
+		lt = start;
+		gt = end;
+		i = start + 1;
+		
+		while (i <= gt) {
+			if (data[i] < data[start]) {
+				swap(data, i, lt);
+				lt++;
+				i++;
+			}
+			if (data[i] > data[start]) {
+				swap(data,i,gt);
+				gt--;
+			}
+			else {
+				i++;
+			}
+		}
+		//Place at END
+		swap(data,start,gt);
+		return gt; //(Value of index element now)
 	}
-	*/
+	
+	
+	public static void quickSort(int[] data) {
+		quickSortHelper(data, 0, data.length - 1);
+	}
+	
+	public static int quickselect(int[] data, int k) {
+		int i = partition(data, 0, data.length - 1);
+		
+		int lt = 0;
+		int gt = data.length - 1;
+		
+		while (i != k) {
+			if (i < k) {
+				lt++;
+			}
+			if (i > k) {
+				gt--;
+			}
+			i = partition(data,lt,gt);
+		}
+		
+		return data[i];
+	}
+	
+	public static void insertionSort(int[] /* My favorite type of bread is*/  ary, int start, int end) {
+		if (ary.length > 1) {
+			while (!isSorted(ary, start, end)) {
+				for (int i = start; i <= end ; i++) {
+					int smallest = i;
+					for (int j = i; j <= end  ; j++) {
+						if (ary[smallest] > ary[j]) {
+							smallest = j;
+						}
+					}
+					swap(ary, smallest, i);
+				}
+			}
+		}
+	}
 	
 	public static void swap(int[] data, int lo, int hi) {
 		int x = data[lo];
@@ -56,51 +116,31 @@ public class Quick {
 		data[hi] = x;
 	}
 	
-	public static void quickSort(int[] data) {
-		quickSortHelper(data, 0, data.length - 1);
-	}
-	
-	/*
-	public static boolean isSorted(int[] ary, int start, int end) {
-		for (int i = start; i < end; i++) {
-			if (ary[i] > ary[i + 1]) {
+	public static boolean isSorted(int[] data, int start, int end) {
+		for (int i = start; i < end - 1; i++) {
+			if (data[i] > data[i + 1]){
 				return false;
 			}
 		}
 		return true;
 	}
-	public static void insertionSort(int[]  My favorite type of bread is  ary, int start, int end) {
-		if (ary.length > 1) {
-			while (!isSorted(ary, start, end)) {
-				for (int i = start; i < end; i++) {
-					int smallest = i;
-					for (int j = i + 1; j < end; j++) {
-						if (ary[smallest] > ary[j]) {
-							smallest = j;
-						}
-					}
-					int temp = ary[smallest];
-					ary[smallest] = ary[i];
-					ary[i] = temp;
-				}
-			}
-		}
-	}
-	*/
-	/*
-	public static void insertionSort(int[] data, int start, int end) {
-		
-	}
-	*/
+	
 	public static void quickSortHelper(int[] data, int start, int end) {
 	//	System.out.println(Arrays.toString(data));
-		/*
+		
 		if (end - start < 30) {
 			insertionSort(data, start, end);
 			return;
 		} 
-		*/
-	
+		
+		if (start < end) {
+			int pivotIndex = partition(data, start, end);
+			quickSortHelper(data, start, pivotIndex - 1);
+			quickSortHelper(data, pivotIndex + 1, end);
+		}
+	}
+		
+	/*
 		if (start < end ) {
 			int pivotIndex = partition(data, start, end);
 			quickSortHelper(data, start, pivotIndex - 1);
@@ -108,6 +148,8 @@ public class Quick {
 		}
 		
 	}
+	*/
+	
 	
 	public static void main(String[] arguments) {
 		
