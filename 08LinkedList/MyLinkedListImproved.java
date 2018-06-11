61101990d3bfc3736c90ad5d24b36669fbab0bfb
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-06-11 06:00:16 -0400

    Update MyLinkedListImproved.java

--- 08LinkedList/MyLinkedListImproved.java
+++ 08LinkedList/MyLinkedListImproved.java
@@ -1,305 +1,264 @@
 import java.util.*;
+
+
+//Actual Linked List:
 public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T>{
-    
-    private Node start, end;
-    private int size;
-
-    public Iterator<T> iterator(){
-	return new LLIterator(start);
-    }
-    //c+p'ed superarray (mostly)
-    public class LLIterator implements Iterator<T>{
-	//Where next is: (Bad naming)
-	Node current;
 	
-	public LLIterator(Node start){
-	    current = start;
+	public Iterator<T> iterator(){
+		return new LLIterator(start);
 	}
-
-	public boolean hasNext(){
-	    return current != null;
+	//c + p'ed superarray mostly
+	public class LLIterator implements Iterator<T>{
+		Node current;
+		public LLIterator(Node start) {
+			current = start;
+		}
+		public boolean hasNext() {
+			return current != null;
+		}
+		public T next() {
+			if (hasNext()) {
+				current = current.getNext();
+			}
+			else {
+				throw new UnsupportedOperationException();
+			}
+			return current.getPrev().getValue();
+		}
 	}
-
-	public T next(){
-	    if (hasNext()){
-		current = current.getNext();
-	    }
-	    else{
-		throw new UnsupportedOperationException();
-	    }
-	    //AKA current
-	    return current.getPrev().getValue();
-	}	
-    }
-    public int max(){
-	if (size == 0){
-	    return -1;
-	}
-	Node current = start;
-	Node max = current;
-	while (current != null){
-	    //CompareTo bc. Integer class
-	    if (current.getValue().compareTo(max.getValue()) >= 0){
-		max = current;
-	    }
-	    current = current.getNext();
-	}
-	return this.indexOf(max.getValue());
-    }
-       
-    public int min(){
-	if (size == 0){
-	    return -1;
-	}
-	Node current = start;
-	Node min = current;
-	while (current != null){
-	    //CompareTo bc. Integer class
-	    if (current.getValue().compareTo(min.getValue()) <= 0){
-		min = current;
-	    }
-	    current = current.getNext();
-	}
-	return this.indexOf(min.getValue());
-    }
-
-    public void extend(MyLinkedListImproved<T> other){
-	end.setNext(other.start);
-	other.start.setPrev(end);
-	end = other.end;
-	size = size + other.size();
-	other.clear();
-    }
+	private class Node{
+		private T value;
+		private Node prev, next;
 		
-    
-    private Node getNode(int index){
-	int counter = 0;
-	Node currentNode = start;
-	while (currentNode != null){
-	    if (counter == index){
-		return currentNode;
-	    }
-	    counter++;
-	    currentNode = currentNode.getNext();
+		//Constructors
+		public Node() {
+			prev = null;
+			next = null;
+		}
+		public Node(T val) {
+			value = val;
+			prev = null;
+			next = null;
+		}
+		
+		//Getters/Setters
+		public Node getPrev() {
+			return prev;
+		}
+		public Node getNext() {
+			return next;
+		}
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		public void setValue(T value) {
+			this.value = value;
+		}
+		public T getValue() {
+			return value;
+		}
+		
+		public String toString() {
+			String ans = "" + getValue();
+			return ans;
+		}
+		
+		public boolean hasNext() {
+			return (getNext() != null);
+		}
+		public boolean hasPrev() {
+			return (getPrev() != null);
+		}
+		
 	}
-	return null;
-    }
-
-    public MyLinkedListImproved(){
-	clear();
-    }
-
-    public String toString(){
-	String ans = new String("[");
-	int counter = 0;
-	Node currentNode = start;
-	while (counter < size && currentNode != null){
-	    ans += currentNode.getValue();
-	    if (currentNode.hasNext()){
-		ans += ", ";
-	    }
-	    else{
+	private Node start, end;
+	private int size;
+	
+	public MyLinkedListImproved() {
+		clear();
+	}
+	
+	public void clear() {
+		start = null;
+		end = null;
+		size = 0;
+	}
+	
+	public String toString() {
+		Node currentNode = start;
+		String ans = "[";
+		while (currentNode != null) {
+			ans += currentNode.toString() + " ";
+			currentNode = currentNode.getNext();
+		}
 		ans += "]";
-	    }
-	    currentNode = currentNode.getNext();
-	    counter++;
-	}
-	return ans;	
-    }
-
-    public void clear(){
-	start = null;
-	end = null;
-	size = 0;
-    }
-
-    public int size(){
-	return size;
-    }
-
-    public T get(int index){
-	//EXCEPTIONS!
-	if (index < 0){
-	    throw new IndexOutOfBoundsException("Too small.");
-	}
-	if (index >= size){
-	    throw new IndexOutOfBoundsException("Too big!");
-	}
-        Node x = getNode(index);
-	return x.getValue();
-    }
-
-    public T set(int index, T value){
-	//EXCEPTIONS!
-	if (index < 0){
-	    throw new IndexOutOfBoundsException("Too small.");
-	}
-	if (index >= size){
-	    throw new IndexOutOfBoundsException("Too big!");
-	}
-	T oldVal = getNode(index).getValue();
-	Node currentNode = start;
-	for (int i = 0; i < index; i++){
-	    currentNode = currentNode.getNext();
-	}
-	currentNode.setValue(value);
-	return oldVal;
-    }
-
-    public int indexOf(T value){
-	int counter = 0;
-	Node currentNode = start;
-	while (!currentNode.getValue().equals(value)){
-	    currentNode = currentNode.getNext();
-	    counter++;
-	}
-	//Not there
-	if (size - 1 == counter){
-	    return -1;
-	}
-	//Is there
-	return counter;
-    }
-
-    //Adds to end pretty sure, from documentation
-    public boolean add(T newData){
-	Node x = new Node(newData);
-	if (size == 0){
-	    start = x;
-	    end = x;
-	}
-	else{
-	    end.setNext(x);
-	    x.setPrev(end);
-	    end = x;
-	}
-	size++;
-	return true;
-    }
-
-    public void add(int index, T value){
-	//EXCEPTIONS!
-	if (index < 0){
-	    throw new IndexOutOfBoundsException("Too small.");
-	}
-	if (index >= size){
-	    throw new IndexOutOfBoundsException("Too big!");
-	}
-	Node newData = new Node(value);
-	//Add to beginning
-	if (index == 0){
-	    start.setPrev(newData);
-	    newData.setNext(start);
-	    start = newData;
-	}
-	//Add to end
-	if (index == size()-1){
-	    end.setNext(newData);
-	    newData.setPrev(end);
-	    end = newData;
-	}
-	//Neither:
-	else{
-	    Node prev = getNode(index - 1);
-	    Node next = getNode(index + 1);
-	    newData.setPrev(prev);
-	    newData.setNext(next);
-	    prev.setNext(newData); //(Maybe?)
-	    //Not maybe, definite yes
-	}
-    }
-
-    //Goes through entire data structure
-    public boolean remove(T value){
-
-	if (indexOf(value) == -1){
-	    return false;
-	}
-	remove(indexOf(value));
-	return true;
-	
-    }
-
-    public T remove(int index){
-	//SHould always be able to remove if the exceptions don't catch it, , so always returns true I think.
-	//EXCEPTIONS!
-	if (index < 0){
-	    throw new IndexOutOfBoundsException("too small.");
-	}
-	if (index >= size){
-	    throw new IndexOutOfBoundsException("TOO BIG!");
-	}
-	//	Integer oldValue = new Integer(getNode(index).getValue());
-	Node oldNode = getNode(index);
-	Node currentNode = getNode(index);
-	
-	/*We forget about the element at this index faster than...
-	  someone giving the silent treatment? */
-	if (index == 0){
-	    T oldValue = start.getValue();
-	    start = start.getNext();
-	    start.setPrev(null);
-	    return oldValue;
-	}
-	if (index == size - 1){
-	    T oldValue = end.getValue();
-	    end = end.getPrev();
-	    end.setNext(null);	    
-	}
-	else{
-	    //CurrentNode: "Doooon't forget me, I beeeeeeeeg"
-	    currentNode.getPrev().setNext(currentNode.getNext());
-	    currentNode.getNext().setPrev(currentNode.getPrev());
-	}
-	size--;
-	return oldNode.getValue();
-    }
-	
-
-    //Node class:
-
-    private class Node{
-	private T value;
-	private Node prev, next;
-	
-	public Node(){
-	    prev = null;
-	    next = null;
+		return ans;
 	}
 	
-	public Node(T val){
-	    value = val;
-	    prev = null;
-	    next = null;
+	private Node getNode(int index) {
+		int counter = 0;
+		Node currentNode = start;
+		while (currentNode != null) {
+			if (counter == index) {
+				return currentNode;
+			}
+			counter++;
+			currentNode = currentNode.getNext();
+		}
+		return null;
 	}
 	
-	public Node getPrev(){
-	    return prev;
-	}
-	public Node getNext(){
-	    return next;
-	}
-	public T getValue(){
-	    return value;
-	}
-	public void setPrev(Node prev){
-	    this.prev = prev;
-	}
-	public void setNext(Node next){
-	    this.next = next;
-	}
-	public void setValue(T value){
-	    this.value = value;
-	}
-	public String toString(){
-	    String ans = new String("" + getValue());
-	    return ans;
+	public int size() {
+		return size;
 	}
 	
-	//Helper function that is very necessary but I don't think was mentioned
-	public boolean hasNext(){
-	    return (getNext() != null);
-	}	
-
-    }
+	public T get(int index) {
+		if (index < 0) {
+			throw new IndexOutOfBoundsException("Too small.");
+		}
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("Too big.");
+		}
+		Node x = getNode(index);
+		return x.getValue();
+	}
+	
+	public T set(int index, T value) {
+		if (index < 0) {
+			throw new IndexOutOfBoundsException("Too small.");
+		}
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("Too big.");
+		}
+		getNode(index).setValue(value);
+		return value;
+	}
+	
+	public int indexOf(T value) {
+		Node currentNode = start;
+		int counter = 0;
+		
+		while (currentNode != null) {
+			if (currentNode.getValue().equals(value)) {
+				return counter;
+			}
+			counter++;
+			currentNode = currentNode.getNext();
+		}
+		return -1;
+	}
+	
+	//Adds to end of LinkedList
+	public boolean add(T newData) {
+		Node x = new Node(newData);
+		if (size == 0) {
+			start = x;
+			end = x;
+		}
+		else {
+			end.setNext(x);
+			x.setPrev(end);
+			end = x;
+		}
+		size++;
+		return true;
+	}
+	//Adds at specific area
+	public boolean add(int index, T value) {
+		if (index < 0) {
+			throw new IndexOutOfBoundsException("Too small.");
+		}
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("Too big.");
+		}
+		
+		Node n = new Node(value);
+		
+		if (index == 0) {
+			start.setPrev(n);
+			n.setNext(start);
+			start.setValue(value);
+		}
+		if (index == size - 1) {
+			return add(value);
+		}
+		
+		else {
+			Node prev = getNode(index - 1);
+			Node next = getNode(index );
+			n.setPrev(prev);
+			n.setNext(next);
+			prev.setNext(n);
+			next.setPrev(n);
+		}
+		size++;
+		return true;
+	}
+	
+	public boolean remove(T value) {
+		if (indexOf(value) == -1) {
+			return false;
+		}
+		remove(indexOf(value));
+		return true;
+	}
+	//Mafia-esque "forgetting"
+	public T remove(int index) {
+		T oldValue;
+		//Start and end:
+		if (index == 0) {
+			oldValue = start.getValue();
+			start = start.getNext();
+			start.setPrev(null);
+			return oldValue;
+		}
+		if (index == size - 1) {
+			oldValue = end.getValue();
+			end = end.getPrev();
+			end.setNext(null);
+			return oldValue;
+		}
+		else {
+			oldValue = get(index);
+			Node currentNodePrev = getNode(index - 1);
+			Node currentNodeNext = getNode(index + 1);
+			currentNodePrev.setNext(currentNodeNext);
+			currentNodeNext.setPrev(currentNodePrev);
+		}
+		size--;
+		return oldValue;
+	}
+	public int max() {
+		if (size == 1) {
+			return 0;
+		}
+		T val = start.getValue();
+		int max = 0;
+		int index = 0;
+		for (T iter : this) {
+			if (iter.compareTo(val) > 0) {
+				val = iter;
+				max = index;
+			}
+			index++;
+		}
+		return index;
+	}
+	public void extend(MyLinkedListImproved<T> other) {
+		if (size() == 0) {
+			start = other.start;
+			return;
+		}
+		if (other.size() == 0) {
+			return;
+		}
+		end.setNext(other.start);
+		other.start.setPrev(end);
+		end = other.end;
+		size += other.size();
+		other.clear();
+	}
 }
